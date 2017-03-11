d3cafd85d47cecc1569204bfd53f8335aae5e1d3
mingzhang <mingzhang@deepglint.com>
2017-03-11 11:08:41 +0800

    update the lr schedule.

--- Train.py
+++ Train.py
@@ -5,7 +5,7 @@ import mxnet as mx
 from DataIter import CarReID_Iter, CarReID_Softmax_Iter
 from Solver import CarReID_Solver, CarReID_Softmax_Solver
 from MDL_PARAM import model2 as now_model
-from MDL_PARAM import model1_softmax as softmax_model
+from MDL_PARAM import model2_softmax as softmax_model
 
 def Do_Train():
   print 'Training...'
@@ -34,7 +34,7 @@ def Do_Train():
 #  solver = CarReID_Solver(param_prefix, reid_net, ctx, data_shape, label_shape, num_epoch, 
 #                    momentum=0.9, wd=0.0005, learning_rate=0.001, lr_scheduler=lr_scheduler)
   solver = CarReID_Solver(param_prefix, reid_net, ctx, data_shape, label_shape, num_epoch, 
-                    opt_method='adam', wd=0.0005, learning_rate=0.001, lr_scheduler=lr_scheduler)
+                    opt_method='adam', wd=0.0005, learning_rate=0.1, lr_scheduler=lr_scheduler)
 
 
   print 'fitting...'
@@ -51,12 +51,12 @@ def Do_Softmax_Train():
   logger = logging.getLogger()
   logger.setLevel(logging.INFO)
   
-  ctx = mx.gpu(3)
+  ctx = mx.gpu(1)
 
   num_epoch = 10000
-  batch_size = 16
+  batch_size = 32
   data_shape = (batch_size, 3, 299, 299)
-  data_shape = (batch_size, 3, 256, 256)
+#  data_shape = (batch_size, 3, 256, 256)
   label_shape = (batch_size, )
   datafn = '/home/mingzhang/data/car_ReID_for_zhangming/data_each.list' #43928 calss number.
 #  datafn = '/home/mingzhang/data/car_ReID_for_zhangming/data_each.10.list'
@@ -64,21 +64,27 @@ def Do_Softmax_Train():
   clsnum=43928
   reid_net = softmax_model.CreateModel_Color(ctx, batch_size, data_shape[2:], clsnum)
   
-  dlr = 200000/batch_size
+  dlr = 700000/batch_size
 #  dlr_steps = [dlr, dlr*2, dlr*3, dlr*4]
-  dlr_steps = [dlr*i for i in xrange(1, 80)]
+  lr_start = 10**-5
+  lr_min = 10**-6
+  lr_reduce = 0.1
+  lr_stepnum = np.log(lr_min/lr_start)/np.log(0.1)
+  lr_stepnum = np.int(np.ceil(lr_stepnum))
+  dlr_steps = [dlr*i for i in xrange(1, lr_stepnum+1)]
+  print 'lr_start:%.1e, lr_min:%.1e, lr_reduce:%.2f, lr_stepsnum:%d'%(lr_start, lr_min, lr_reduce, lr_stepnum)
   print dlr_steps
-  lr_scheduler = mx.lr_scheduler.MultiFactorScheduler(dlr_steps, 0.9)
+  lr_scheduler = mx.lr_scheduler.MultiFactorScheduler(dlr_steps, lr_reduce)
 #  lr_scheduler = mx.lr_scheduler.FactorScheduler(dlr, 0.9)
-  param_prefix = 'MDL_PARAM/params1_softmax/car_reid'
-  solver = CarReID_Softmax_Solver(param_prefix, reid_net, ctx, data_shape, label_shape, num_epoch, 
-                    momentum=0.9, wd=0.0005, learning_rate=0.0002, lr_scheduler=lr_scheduler)
+  param_prefix = 'MDL_PARAM/params2_softmax/car_reid'
 #  solver = CarReID_Softmax_Solver(param_prefix, reid_net, ctx, data_shape, label_shape, num_epoch, 
-#                    opt_method='rmsprop', wd=0.0005, learning_rate=0.01, lr_scheduler=lr_scheduler)
+#                    momentum=0.9, wd=0.0005, learning_rate=0.01, lr_scheduler=lr_scheduler)
+  solver = CarReID_Softmax_Solver(param_prefix, reid_net, ctx, data_shape, label_shape, num_epoch, 
+                    opt_method='rmsprop', wd=0.0005, learning_rate=lr_start, lr_scheduler=lr_scheduler)
 
 
   print 'fitting...'
-  resotre_whichone = 0
+  resotre_whichone = 6
   solver.fit(data_train, showperiod=100, whichone=resotre_whichone, logger=logger) 
   print 'over...'
 
